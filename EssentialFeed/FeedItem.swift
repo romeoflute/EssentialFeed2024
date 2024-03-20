aee5e0749750b0da58128a210e1ef003dfeb3d76
Romeo Flauta <meoflauta@gmail.com>
2024-03-20 14:56:23 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 3/20/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
