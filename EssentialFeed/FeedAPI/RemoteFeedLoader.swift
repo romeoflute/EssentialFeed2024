28d55de34a280b84f9f4755b108d952075977cec
Romeo Flauta <meoflauta@gmail.com>
2024-04-01 21:58:44 +0800

    Move all mapping logic to the FeedItemsMapper

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -31,12 +31,7 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                do {
-                    let items = try FeedItemsMapper.map(data, response)
-                    completion(.success(items))
-                } catch {
-                    completion(.failure(.invalidData))
-                }
+                completion(FeedItemsMapper.map(data, from: response))
             case .failure:
                 // map from Swift Error to custom Error
                 // lesson: client error should be mapped from Error in API Client to own domain Error in Loader class
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -105,6 +105,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        // Lesson: detect memory; will fire once after test is fired during teardown
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
         }
