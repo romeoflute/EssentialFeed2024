0f7bb9551f3df21f041e720f6d44896b11d9a078
Romeo Flauta <meoflauta@gmail.com>
2024-04-01 21:26:25 +0800

    Add comment

--- EssentialFeed/FeedFeature/FeedItem.swift
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -14,6 +14,7 @@ public struct FeedItem: Equatable {
     public let location: String?
     public let imageURL: URL
     
+    // even if struct is public, we need to have a public init to access it in the tests
     public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
         self.description = description
