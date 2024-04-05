ee9301edc156969dff9b030a451a73affb46fb96
Romeo Flauta <meoflauta@gmail.com>
2024-04-05 15:02:22 +0800

    Extract cache deletion side-effects on expired cache from the load method to the  method

--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -103,7 +103,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
     
-    func test_load_deletesCacheOnMoreThanSevenDaysOldCache() {
+    func test_load_hasNoSideEffectsOnMoreThanSevenDaysOldCache() {
 		let feed = uniqueImageFeed()
 		let fixedCurrentDate = Date()
 		let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
@@ -112,7 +112,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		sut.load { _ in }
 		store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
 
-		XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
     
     func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
