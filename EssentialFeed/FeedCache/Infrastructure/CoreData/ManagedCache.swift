12a6fddcce86816cf202feed607fe2da02e8e55d
Romeo Flauta <meoflauta@gmail.com>
2024-04-08 15:59:42 +0800

    Extract CoreData managed model classes into separate files

---
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,42 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 4/8/24.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+
+}
+
+extension ManagedCache {
+
+    @nonobjc private class func fetchRequest() -> NSFetchRequest<ManagedCache> {
+        return NSFetchRequest<ManagedCache>(entityName: "ManagedCache")
+    }
+
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+		let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+		request.returnsObjectsAsFaults = false
+		return try context.fetch(request).first
+	}
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+		try find(in: context).map(context.delete)
+		return ManagedCache(context: context)
+	}
+
+    var localFeed: [LocalFeedImage] {
+		return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+	}
+}
+
+extension ManagedCache : Identifiable {
+
+}
---
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,46 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 4/8/24.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+
+}
+
+extension ManagedFeedImage {
+
+    @nonobjc private class func fetchRequest() -> NSFetchRequest<ManagedFeedImage> {
+        return NSFetchRequest<ManagedFeedImage>(entityName: "ManagedFeedImage")
+    }
+
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: localFeed.map { local in
+			let managed = ManagedFeedImage(context: context)
+			managed.id = local.id
+			managed.imageDescription = local.description
+			managed.location = local.location
+			managed.url = local.url
+			return managed
+		})
+	}
+
+	var local: LocalFeedImage {
+		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+	}
+
+}
+
+extension ManagedFeedImage : Identifiable {
+
+}
