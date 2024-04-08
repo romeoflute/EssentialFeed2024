c031acb2024f479b65e08fb3544deb960a932cd8
Romeo Flauta <meoflauta@gmail.com>
2024-04-08 13:30:01 +0800

    CoreDataFeedStore.retrieve() delivers empty on empty cache

---
+++ EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 4/8/24.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+	public init() {}
+
+	public func retrieve(completion: @escaping RetrievalCompletion) {
+		completion(.empty)
+	}
+
+	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+	}
+
+	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+	}
+}
