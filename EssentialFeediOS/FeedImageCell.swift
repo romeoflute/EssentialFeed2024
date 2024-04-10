a53d7fc4aff94174c44d1502fdb3835809feefe8
Romeo Flauta <meoflauta@gmail.com>
2024-04-10 22:12:05 +0800

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Romeo Flauta on 4/10/24.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+}
