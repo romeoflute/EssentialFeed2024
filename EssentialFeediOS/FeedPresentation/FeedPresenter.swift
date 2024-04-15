0944348825a521c457c31ca92271f3f350daaffe
Romeo Flauta <meoflauta@gmail.com>
2024-04-15 14:24:45 +0800

    Localize feed view title string

--- EssentialFeediOS/FeedPresentation/FeedPresenter.swift
+++ EssentialFeediOS/FeedPresentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by Romeo Flauta on 4/14/24.
 //
 
+import Foundation
 import EssentialFeed
 
 protocol FeedLoadingView {
@@ -25,7 +26,10 @@ final class FeedPresenter {
 	}
     
     static var title: String {
-		return "My Feed"
+		return NSLocalizedString("FEED_VIEW_TITLE",
+			tableName: "Feed",
+			bundle: Bundle(for: FeedPresenter.self),
+			comment: "Title for the feed view")
 	}
 
     func didStartLoadingFeed() {
