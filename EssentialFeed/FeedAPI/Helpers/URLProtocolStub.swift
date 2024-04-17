df070aa0740d160054257666cf5a40a65b1f3bbb
Romeo Flauta <meoflauta@gmail.com>
2024-04-17 21:54:24 +0800

    Rename test case class to reflect the use case it refers to

--- EssentialFeed/FeedAPI/Helpers/URLProtocolStub.swift
+++ EssentialFeed/FeedAPI/Helpers/URLProtocolStub.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class URLProtocolStub: URLProtocol {
+public class URLProtocolStub: URLProtocol {
 	private struct Stub {
 		let data: Data?
 		let response: URLResponse?
@@ -23,27 +23,27 @@ class URLProtocolStub: URLProtocol {
 
 	private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
 
-	static func stub(data: Data?, response: URLResponse?, error: Error?) {
+	public static func stub(data: Data?, response: URLResponse?, error: Error?) {
 		stub = Stub(data: data, response: response, error: error, requestObserver: nil)
 	}
 
-	static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+	public static func observeRequests(observer: @escaping (URLRequest) -> Void) {
 		stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
 	}
 
-	static func removeStub() {
+	public static func removeStub() {
 		stub = nil
 	}
 
-	override class func canInit(with request: URLRequest) -> Bool {
+    public override class func canInit(with request: URLRequest) -> Bool {
 		return true
 	}
 
-	override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+    public override class func canonicalRequest(for request: URLRequest) -> URLRequest {
 		return request
 	}
 
-	override func startLoading() {
+    public override func startLoading() {
 		guard let stub = URLProtocolStub.stub else { return }
 
 		if let data = stub.data {
@@ -63,5 +63,5 @@ class URLProtocolStub: URLProtocol {
 		stub.requestObserver?(request)
 	}
 
-	override func stopLoading() {}
+    public override func stopLoading() {}
 }
