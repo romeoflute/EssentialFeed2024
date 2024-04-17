05407aa3ac25963803ad5810fa352bce0baaef8d
Romeo Flauta <meoflauta@gmail.com>
2024-04-17 14:26:32 +0800

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 4/17/24.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+	public let isLoading: Bool
+}
