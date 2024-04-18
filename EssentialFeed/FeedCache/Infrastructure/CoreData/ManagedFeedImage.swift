b5fba8addae3a4688f0122a4788b36779c0308f7
Romeo Flauta <meoflauta@gmail.com>
2024-04-18 15:33:05 +0800

    Add new FeedStore core data model version adding an optional Data property to the `ManagedFeedImage`

--- EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -22,6 +22,7 @@ extension ManagedFeedImage {
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
     @NSManaged var url: URL
+    @NSManaged var data: Data?
     @NSManaged var cache: ManagedCache
 }
 
