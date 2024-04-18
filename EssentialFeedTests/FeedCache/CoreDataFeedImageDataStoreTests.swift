9fc951a585d16ff09efdec26ec7984cd4ca6f624
Romeo Flauta <meoflauta@gmail.com>
2024-04-18 15:40:43 +0800

    CoreDataFeedStore.retrieveImageData delivers stored data when there's an image with a matching URL in the store

--- EssentialFeedTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
@@ -25,6 +25,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 
 		expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
 	}
+    
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
+		let sut = makeSUT()
+		let storedData = anyData()
+		let matchingURL = URL(string: "http://a-url.com")!
+
+		insert(storedData, for: matchingURL, into: sut)
+
+		expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
+	}
 
 	// - MARK: Helpers
 
@@ -34,6 +44,10 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 		let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
+	}
+    
+    private func found(_ data: Data) -> FeedImageDataStore.RetrievalResult {
+		return .success(data)
 	}
 
 	private func notFound() -> FeedImageDataStore.RetrievalResult {
