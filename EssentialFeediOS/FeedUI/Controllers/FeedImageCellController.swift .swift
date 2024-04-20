5f94f3d81c3e0874d02d2b1dfc7e5c8798fc79be
Romeo Flauta <meoflauta@gmail.com>
2024-04-20 21:53:17 +0800

    Update accessibility identifiers of cell

--- EssentialFeediOS/FeedUI/Controllers/FeedImageCellController.swift .swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedImageCellController.swift .swift
@@ -23,6 +23,7 @@ final class FeedImageCellController: FeedImageView {
 
     func view(in tableView: UITableView) -> UITableViewCell {
         cell = tableView.dequeueReusableCell()
+        setAccessibility()
         delegate.didRequestImage()
 		return cell!
 	}
@@ -41,12 +42,16 @@ final class FeedImageCellController: FeedImageView {
 		cell?.locationLabel.text = viewModel.location
 		cell?.descriptionLabel.text = viewModel.description
         cell?.feedImageView.setImageAnimated(viewModel.image)
-        cell?.feedImageView.accessibilityIdentifier = "FeedImage"
         cell?.feedImageView.tag = viewModel.bytes ?? 0
 		cell?.feedImageContainer.isShimmering = viewModel.isLoading
 		cell?.feedImageRetryButton.isHidden = !viewModel.shouldRetry
 		cell?.onRetry = delegate.didRequestImage
 	}
+    
+    private func setAccessibility() {
+        cell?.accessibilityIdentifier = "FeedImageCell"
+        cell?.feedImageView.accessibilityIdentifier = "FeedImageView"
+    }
     
     private func releaseCellForReuse() {
 		cell = nil
